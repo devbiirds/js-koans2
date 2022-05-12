b0d4a755146e5b46f48197456d416f1d6273fdfd
devbiirds <vadikoff57@gmail.com>
2022-05-12 19:28:32 +0300

    all tasks solved

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -2,13 +2,16 @@
 module("About Asserts (topics/about_asserts.js)");
 
 test("ok", function() {
+    const __ = true;
     ok(__ === true, 'what will satisfy the ok assertion?');
 });
 
 test("not ok", function() {
+    const __ = false;
     ok(__ === false, 'what is a false value?');
 });
 
 test("equal", function() {
+    const __ = 2;
     equal(__, 1 + 1, 'what will satisfy the equal assertion?');
 });
